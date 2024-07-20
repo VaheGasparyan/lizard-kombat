b3bec5b200000ee870878f207b2f18a6c7511e35
VaheGasparyan <vahegasparyan25@gmail.com>
2024-07-20 18:02:05 +0400

    create add plus animation

---
+++ script.js
@@ -0,0 +1,41 @@
+const $hero = document.getElementById('hero');
+const click__element__wrapper = document.querySelector('.click__element__wrapper');
+
+$hero.addEventListener('click', (event) => {
+    const DEG = 40;
+
+    const {
+        height,
+        width,
+        x,
+        y,
+        top,
+        left,
+    } = $hero.getBoundingClientRect();
+
+    const offsetX = event.clientX - left - width / 2;
+    const offsetY = event.clientY - top - height / 2;
+
+    const tiltX = (offsetY / height) * DEG;
+    const tiltY = (offsetX / width) * -DEG;
+
+    $hero.style.setProperty('--tiltX', `${tiltX}deg`);
+    $hero.style.setProperty('--tiltY', `${tiltY}deg`);
+
+    setTimeout(() => {
+        $hero.style.setProperty('--tiltX', '0deg');
+        $hero.style.setProperty('--tiltY', '0deg');
+    }, 300);
+
+    const $plus_one = document.createElement('span');
+    $plus_one.innerText = '+1';
+    $plus_one.id = 'plus_one';
+    $plus_one.style.left = `${event.clientX - left}px`;
+    $plus_one.style.top = `${event.clientY - top}px`;
+
+    click__element__wrapper.appendChild($plus_one);
+
+    setTimeout(() => {
+        $plus_one.remove()
+    }, 2000)
+})
